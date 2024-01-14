01c5cd5b0f8daca4243d8174354cc8fc1943f1fe
rahilscode <135556049+rahilscode@users.noreply.github.com>
2024-01-14 23:45:49 +0000

    this contains datatypes information

---
+++ Basics/Variables.js
@@ -0,0 +1,18 @@
+let name1="Rahil"
+const value= 1234
+var name2= "Rahil Sharma Kidda"
+//if the value is not defined then it is undefined
+let unValue
+
+name="Sahil"
+name2="kumar"
+
+/*NOTE:  don't use var, only use let due to block and functionl scope
+value=321 constant value can not be changed*/
+
+console.log(name1);
+console.log(name2);
+console.log(value)
+
+//Through this you can display all values
+console.table([name, name2, value, unValue])
---
+++ Basics/datatypes.js
@@ -0,0 +1,18 @@
+//By mentioning this, newer rules and procedures will be applied to the js code
+"use strict"
+
+//this  can not printed bcz we are using node of the execution
+//alert("Kidaa")
+
+let StringValue ="Rahil"
+let booleanValue= true
+let numberValue= 13254 //this is further subdivided into bigint for larger number value
+let nullValue= null //this will show as Object, it is Language error
+let unValue
+
+console.log(typeof StringValue)
+console.log(typeof booleanValue)
+console.log(typeof numberValue)
+console.log(typeof nullValue)
+console.log(typeof unValue)
+console.log(typeof NaN) //This will display as Number and NAN stands for Not A Number
